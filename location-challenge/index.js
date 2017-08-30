2c034ad8489e454db97a06901cb905515791259f
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-08-30 16:00:24 -0700

    Write a function using .map() to filter for coordinate property of all companies. #3

--- location-challenge/index.js
+++ location-challenge/index.js
@@ -2,6 +2,11 @@ var https = require('https')
 
 const url = 'https://success.spidergap.com/partners.json?inf_contact_key=63bcaf2e5edf052bc69ffb6e9a34ff92ddf05a0e332f2096f1521d127943b21b'
 
+function filterLocation(companies) {
+  companies.map(company => {
+    console.log(company.offices[0].coordinates)
+  })
+}
 https.get(url, (res) => {
   const { statusCode } = res
   const contentType = res.headers['content-type']
@@ -19,7 +24,7 @@ https.get(url, (res) => {
   res.on('end', () => {
     try {
       const parsed = JSON.parse(body)
-      console.log(parsed)
+      console.log(filterLocation(parsed))
     }
     catch (err) {
       console.log(err.message)
