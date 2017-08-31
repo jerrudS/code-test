20fd42e2024ea48be437aae64a58087f0a6635b9
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-08-30 23:37:02 -0700

    Write a function that filters for companies within 100km of (51.515419, -0.141099) #3

---
+++ location-challenge/utilities.js
@@ -0,0 +1,35 @@
+const lat1 = 51.515419
+const lon1 = -0.141099
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371
+  const pi = Math.PI
+  const φ1 = lat1 * (pi / 180)
+  const φ2 = lat2 * (pi / 180)
+  const Δφ = (lat2 - lat1) * (pi / 180)
+  const Δλ = (lon2 - lon1) * (pi / 180)
+
+  const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+  const distance = R * c
+  return distance
+}
+
+function filterLocation(companies) {
+  companies.map(company => {
+    company.offices.map(each => {
+      const coordinates = each.coordinates.split(',')
+      const lat2 = coordinates[0]
+      const lon2 = coordinates[1]
+
+      if (calculateDistance(lat1, lon1, lat2, lon2) <= 100) {
+        const companyName = company.organization
+        const address = each.address
+        console.log(companyName, address)
+      }
+    })
+  })
+}
+
+module.exports = filterLocation
